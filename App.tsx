9063d8b82c6f74924d105f3a9ebfbd66a15d2d8b
Zakky Muhammad Fajar <zakky.muhammad@sicepat.com>
2024-05-15 18:58:06 +0700

    chore: absolute path

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-
-import RootNavigation from './src/navigation/RootNavigation';
+import RootNavigation from '@src/navigation/RootNavigation';
 
 const App = () => {
   return (
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@assets': './src/shared/assets',
+          '@components': './src/shared/components',
+          '@config': './src/shared/config',
+          '@core': './src/shared/core',
+          '@theme': './src/shared/theme',
+          '@base': './src/app/shared/base',
+          '@utils': './src/app/shared/utils',
+          '@constants': './src/app/shared/constants',
+          '@libraries': './src/app/shared/libraries',
+          '@features': './src/features',
+          '@src': './src',
+        },
+      },
+    ],
+  ],
 };
